f9065c56c87d0c5b0e1e2da32d3589fc237216ff
Aljamil1113 <aarazasaljamil@gmail.com>
2020-10-24 21:07:18 -0700

    Initial Project

---
+++ Vidly2_0/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Vidly2_0.Models;
+
+namespace Vidly2_0.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            Movie movie = new Movie()
+            {
+                Id = 1,
+                Name = "James Bond 007"
+            }; 
+            return View(movie);
+        }
+
+        
+    }
+}
---
+++ Vidly2_0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vidly2_0.MvcApplication" Language="C#" %>
